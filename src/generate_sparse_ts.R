4f0d94ce2f42b65740a90c7f2e708c1593e12ac9
dcuoliveira <dan.cunhaa@gmail.com>
2023-01-22 23:36:35 -0300

    exclude constant

--- src/generate_sparse_ts.R
+++ src/generate_sparse_ts.R
@@ -96,7 +96,7 @@ for (g in groups){
             file = file.path(data_files, "simulation", file_name))
 
     # 3. non-linear (in parameter) relationships - TVAR model
-    tvar_fit <- TVAR(target_ts, lag = p, dummyToBothRegimes = TRUE)
+    tvar_fit <- TVAR(target_ts, lag = p, dummyToBothRegimes = TRUE, include = "none")
     B_TVAR <- cbind(tvar_fit$coefficients[["Bdown"]], # nolint
                     tvar_fit$coefficients[["Bup"]])
     file_name <- paste0(g, "_TVAR_B.csv") # nolint
