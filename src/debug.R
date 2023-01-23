98dec7592e8a6984724accf5d8d52fc8af868c13
dcuoliveira <dan.cunhaa@gmail.com>
2023-01-22 23:25:04 -0300

    checkpoint

--- src/debug.R
+++ src/debug.R
@@ -1,3 +1,4 @@
+rm(list = ls())
 library("tsDyn")
 library("reticulate")
 library("dplyr")
@@ -14,5 +15,13 @@ target_file <- file.path(stocks_files, paste0(g, "_stock_indexes.npz"))
 target_data <- np$load(target_file)
 target_ts <- target_data["X_np"] %>% as.data.table()
 
-#estimate the new serie
+# estimate TVAR
 tvar_fit <- TVAR(target_ts, lag = 7, dummyToBothRegimes = TRUE)
+B <- cbind(tvar_fit$coefficients[["Bdown"]], tvar_fit$coefficients[["Bup"]])
+
+sim <- TVAR.sim(B = B,
+                n = 500,
+                lag = 7,
+                mTh = 1,
+                nthresh = 1,
+                Thresh = tvar_fit$model.specific$Thresh)
