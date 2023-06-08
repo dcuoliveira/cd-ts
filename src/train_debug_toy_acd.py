894e13c4a9e569c7b5d8bb11802d73f7d71dfa27
dcuoliveira <dan.cunhaa@gmail.com>
2023-06-08 11:16:13 -0300

    add comments and refactor code

--- src/train_debug_toy_acd.py
+++ src/train_debug_toy_acd.py
@@ -1,3 +1,4 @@
+import os
 from itertools import chain
 import numpy as np
 import torch
@@ -5,30 +6,42 @@ from tqdm import tqdm
 from torch.nn import functional as F
 
 from data_loaders import load_springs_data
-from models.GNNs import NRIMLP
+from models.MLPEncoder import MLPEncoder
 from models.MLPDecoder import MLPDecoder
 from utils.Pyutils import my_softmax, kl_categorical_uniform, edge_accuracy, encode_onehot, expand_edges, get_off_diag_idx
 
-def sample_gumbel(logits, temperature=1.0):
+def sample_gumbel(logits, tau=1.0):
     gumbel_noise = -torch.log(1e-10 - torch.log(torch.rand_like(logits) + 1e-10))
     y = logits + gumbel_noise
-    return torch.softmax(y / temperature, axis=-1)
+    return torch.softmax(y / tau, axis=-1)
 
+# run parameters
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 num_atoms = 3
+dataset_name = "spring_data_test"
+file_name = "_springs{}_l5100_s1000".format(num_atoms) # NOTE: changed to 1000 instead of 10000 (missing edges file for 10000)
+root_path = os.path.dirname(__file__)
+data_path = os.path.join(root_path, "data")
+
 ## Load data
-train_dataset = load_springs_data("src/data/spring_data_test", "_springs{}_l5100_s10000".format(num_atoms), num_atoms=num_atoms)
+train_dataset = load_springs_data(root_dir=os.path.join(data_path, dataset_name),
+                                  suffix=file_name,
+                                  num_atoms=num_atoms)
+# NOTE: Random sampling occurs in the "num_sample" dimension instead of "num_obs"
 train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=256, shuffle=True, num_workers=0)
 off_diag = np.ones((num_atoms, num_atoms)) - np.eye(num_atoms)
 rel_rec = np.array(encode_onehot(np.where(off_diag)[0]), dtype=np.float32)
 rel_send = np.array(encode_onehot(np.where(off_diag)[1]), dtype=np.float32)
 rel_rec = torch.FloatTensor(rel_rec).to(device)
 rel_send = torch.FloatTensor(rel_send).to(device)
+
 ## Load Models
-encoder = NRIMLP(50*4, 128, 2)
+encoder = MLPEncoder(50*4, 128, 2)
 decoder = MLPDecoder(4, 128, 2)
+
 ## Optimizers
 optimizer = torch.optim.Adam(chain(encoder.parameters(), decoder.parameters()), lr=5e-4)
+
 ## Train
 encoder.train()
 decoder.train()
@@ -40,11 +53,14 @@ for i in range(5000):
         optimizer.zero_grad()
         features = features.to(device)
         gt_edges = gt_edges.to(device)
+
+        # train encoder
+        # features: (batch_size, num_objects, num_timesteps, ??)
         logits = encoder.forward(features, rel_rec=rel_rec, rel_send=rel_send)
         prob = my_softmax(logits, -1)
         
         # Gumbel-Softmax sampling
-        edges = sample_gumbel(logits, temperature=0.5)
+        edges = sample_gumbel(logits, tau=0.5)
         # Decoding step
         output = decoder(features, edges, rel_rec=rel_rec, rel_send=rel_send, teacher_forcing=10)
         decode_target = features[:,:,1:]
