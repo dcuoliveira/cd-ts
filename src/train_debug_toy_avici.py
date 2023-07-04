809cb21484763b6290b16ef4e361d91d788771fd
dcuoliveira <dan.cunhaa@gmail.com>
2023-07-04 20:40:53 -0300

    refactor hyperparameters

--- src/train_debug_toy_avici.py
+++ src/train_debug_toy_avici.py
@@ -7,9 +7,23 @@ from models.TransformerEncoder import TransformerEncoder
 from data_loaders import load_springs_data
 from utils.Pyutils import find_gpu_device, encode_onehot
 
+# network hyperparameters
+num_atoms = 5
+dim_val=512
+n_encoder_layers=4
+n_heads=8
+dropout_encoder=0.2
+dropout_pos_enc=0.1
+dim_feedforward_encoder=2048
+
+# optimizer hyperparameters
+n_epochs = 500
+learning_rate = 5e-4
+learning_rate_decay_step = 200
+gamma = 0.5
+
 # running parameters
 device = torch.device(find_gpu_device())
-num_atoms = 5
 dataset_name = "spring_data_test"
 file_name = "_springs{}".format(num_atoms) # NOTE: changed to 1000 instead of 10000 (missing edges file for 10000)
 root_path = os.path.dirname(__file__)
@@ -29,23 +43,23 @@ rel_rec = torch.FloatTensor(rel_rec).to(device)
 rel_send = torch.FloatTensor(rel_send).to(device)
 
 # load Models
-encoder = TransformerEncoder(input_size=1,
-                             batch_first=True,
-                             dim_val=512,  
-                             n_encoder_layers=4,
-                             n_heads=8,
-                             dropout_encoder=0.2, 
-                             dropout_pos_enc=0.1,
-                             dim_feedforward_encoder=2048,
-                             num_predicted_features=1).to(device)
+encoder = TransformerEncoder(input_size=num_atoms,
+                             dim_val=dim_val,  
+                             n_encoder_layers=n_encoder_layers,
+                             n_heads=n_heads,
+                             dropout_encoder=dropout_encoder, 
+                             dropout_pos_enc=dropout_pos_enc,
+                             dim_feedforward_encoder=dim_feedforward_encoder,
+                             num_predicted_features=num_atoms).to(device)
 
 # optimizers
-optimizer = torch.optim.Adam(encoder.parameters(), lr=5e-4)
-scheduler = torch.optim.lr_scheduler.StepLR(optimizer, 200, gamma=0.5)
+optimizer = torch.optim.Adam(encoder.parameters(), lr=learning_rate)
+scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=learning_rate_decay_step, gamma=gamma)
 
 # train
 encoder.train()
-for i in range(500):
+for i in range(n_epochs):
+
     print("Epoch:", i)
     pbar = tqdm(enumerate(train_loader), total=len(train_loader))
     for i, (features, gt_edges) in pbar:
