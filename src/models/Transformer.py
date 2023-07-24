2dd01276af1ad661b30ac0c0c6cceb2a665a9d5f
dcuoliveira <dan.cunhaa@gmail.com>
2023-07-24 17:56:10 -0300

    change input names

--- src/models/Transformer.py
+++ src/models/Transformer.py
@@ -9,7 +9,7 @@ class PositionalEncoder(nn.Module):
         self, 
         dropout: float=0.1, 
         max_seq_len: int=5000, 
-        d_model: int=512,
+        n_out: int=512,
         batch_first: bool=False
         ):
 
@@ -22,19 +22,19 @@ class PositionalEncoder(nn.Module):
 
         super().__init__()
 
-        self.d_model = d_model
+        self.d_model = n_out
         self.batch_first = batch_first
         position = torch.arange(max_seq_len).unsqueeze(1)
-        div_term = torch.exp(torch.arange(0, d_model, 2) * (-math.log(10000.0) / d_model))
+        div_term = torch.exp(torch.arange(0, n_out, 2) * (-math.log(10000.0) / n_out))
 
         self.dropout = nn.Dropout(p=dropout)  
 
         if self.batch_first:
-            pe = torch.zeros(1, max_seq_len, d_model)
+            pe = torch.zeros(1, max_seq_len, n_out)
             pe[0, :, 0::2] = torch.sin(position * div_term)
             pe[0, :, 1::2] = torch.cos(position * div_term)
         else:
-            pe = torch.zeros(max_seq_len, 1, d_model)
+            pe = torch.zeros(max_seq_len, 1, n_out)
             pe[:, 0, 0::2] = torch.sin(position * div_term)
             pe[:, 0, 1::2] = torch.cos(position * div_term)
         
@@ -44,7 +44,7 @@ class PositionalEncoder(nn.Module):
         
         """
         Args:
-            x: Tensor, shape [batch_size, enc_seq_len, dim_val] or [enc_seq_len, batch_size, dim_val]
+            x: Tensor, shape [batch_size, enc_seq_len, n_out] or [enc_seq_len, batch_size, n_out]
         """
 
         if self.batch_first:
@@ -57,8 +57,8 @@ class PositionalEncoder(nn.Module):
 class Transformer(nn.Module):
 
     def __init__(self, 
-        input_size: int,
-        dim_val: int=512,  
+        n_in: int,
+        n_out: int=512,  
         n_encoder_layers: int=4,
         n_heads: int=8,
         dropout_encoder: float=0.2, 
@@ -70,9 +70,9 @@ class Transformer(nn.Module):
         """
         Args:
 
-            input_size: int, number of input variables. 1 if univariate.
-            dim_val: int, aka d_model. All sub-layers in the model produce 
-                     outputs of dimension dim_val
+            n_in: int, number of input variables. 1 if univariate.
+            n_out: int, aka d_model. All sub-layers in the model produce 
+                     outputs of dimension n_out
             n_encoder_layers: int, number of stacked encoder layers in the encoder
             n_heads: int, the number of attention heads (aka parallel attention layers)
             dropout_encoder: float, the dropout rate of the encoder
@@ -85,22 +85,22 @@ class Transformer(nn.Module):
         super().__init__() 
 
         self.encoder_input_layer = nn.Linear(
-            in_features=input_size, 
-            out_features=dim_val 
+            in_features=n_in, 
+            out_features=n_out 
             )
         
         self.linear_mapping = nn.Linear(
-            in_features=dim_val, 
+            in_features=n_out, 
             out_features=num_predicted_features
             )
 
         self.positional_encoding_layer = PositionalEncoder(
-            d_model=dim_val,
+            d_model=n_out,
             dropout=dropout_pos_enc
             )
 
         encoder_layer = nn.TransformerEncoderLayer(
-            d_model=dim_val, 
+            d_model=n_out, 
             nhead=n_heads,
             dim_feedforward=dim_feedforward_encoder,
             dropout=dropout_encoder,
@@ -113,21 +113,21 @@ class Transformer(nn.Module):
             )
 
     def forward(self,
-                src: Tensor) -> Tensor:
+                inputs: Tensor) -> Tensor:
    
         # linear layer
         # output shape: [src_seq_length, batch_size, hidden_dim]
-        src = self.encoder_input_layer(src)
+        x = self.encoder_input_layer(inputs)
         # positional encoding
-        src = self.positional_encoding_layer(src)
+        x = self.positional_encoding_layer(x)
 
         # transformer encoder: muti-head self attention -> add & norm -> feed forward -> add & norm
         # each trasnformer encode consists of a multi-head self attention layer, a feed forward layer, and 2 residual sublayers
         # avici encoder consists of 4 transformer encoders
         # src shape: [src_seq_length, batch_size, hidden_dim]
         # NOTE - need attention mask ?
-        # NOTE - modify self-attention to attend over d and n alternatively (AVICI)
-        encoder_output = self.encoder(src=src) # self-attention attends over d
+        # NOTE - modify self-attention to attend over d or n ?
+        encoder_output = self.encoder(src=x) # self-attention attends over d
         encoder_output = self.encoder(src=encoder_output) # self-attention attends over n
         encoder_output = self.encoder(src=encoder_output) # self-attention attends over d
         encoder_output = self.encoder(src=encoder_output) # self-attention attends over n
