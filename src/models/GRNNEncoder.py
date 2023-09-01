30793768dc7f136dacd1ee138a33e2ce336a6498
Carles Balsells Rodas <carlesbalsellsrodas@hotmail.com>
2023-09-01 14:19:33 +0100

    GRNN Encoder v2

--- src/models/GRNNEncoder.py
+++ src/models/GRNNEncoder.py
@@ -13,18 +13,23 @@ class GRNNEncoder(nn.Module):
     ):
         super(GRNNEncoder, self).__init__()
 
+        self.hidden_dim = n_hid
         self.n_edge_types = n_edge_types
+        self.num_rec_layers = 3
 
         self.mlp1 = MLP(n_in, n_hid, n_hid, do_prob)
-        self.mlp2 = MLP(n_hid * 2, n_hid, n_hid, do_prob)
+        self.mlp2 = MLP(n_hid*2, n_hid, n_hid, do_prob)
         self.mlp3 = MLP(n_hid, n_hid, n_hid, do_prob)
-        self.factor = factor
-        if self.factor:
-            self.mlp4 = MLP(n_hid * 3, n_hid, n_hid, do_prob)
-        else:
-            self.mlp4 = MLP(n_hid * 2, n_hid, n_hid, do_prob)
+        self.mlp4 = MLP(n_hid*2, n_hid, n_hid, do_prob)
+        
+
         if rnn == 'lstm':
-            self.rnn = nn.LSTM(n_hid, n_hid, num_layers=3, batch_first=True)
+            num_rec_layers = 6
+            self.hidden_decoder = nn.LSTMCell(self.hidden_dim, self.hidden_dim)
+            if num_rec_layers!= 1:
+                self.hidden_decoder = nn.ModuleList([nn.LSTMCell(self.hidden_dim, self.hidden_dim)])
+                for _ in range(1, num_rec_layers):
+                    self.hidden_decoder.append(nn.LSTMCell(self.hidden_dim*2, self.hidden_dim))
         elif rnn == 'gru':
             self.rnn = nn.GRU(n_hid, n_hid, num_layers=3, batch_first=True)
         elif rnn == 'lru':
@@ -66,26 +71,25 @@ class GRNNEncoder(nn.Module):
 
         # Input has shape: [num_sims, num_atoms, num_timesteps, num_dims]
         B, N, T, D = inputs.size()
+        R = N*(N-1)
         x = inputs.transpose(1,2).reshape(B*T, N, D)
         # New shape: [num_sims*num_timesteps, num_atoms, num_dims]
         x = self.mlp1(x)  # 2-layer ELU net per node
         x = self.node2edge(x, rel_rec, rel_send)
-        x = self.mlp2(x)
-        x_skip = x
-
-        if self.factor:
-            x = self.edge2node(x, rel_rec, rel_send)
-            x = self.mlp3(x)
-            x = self.node2edge(x, rel_rec, rel_send)
-            x = torch.cat((x, x_skip), dim=2)  # Skip connection
-            x = self.mlp4(x)
-        #else:
-            #x = self.mlp3(x)
-            #x = torch.cat((x, x_skip), dim=2)  # Skip connection
-            #x = self.mlp4(x)
-        R = x.size(1)
-        x = x.reshape(B,T, R, -1).transpose(1,2).reshape(B*R,T,-1)
-        # New shape: [num_sims*num_edges, num_timesteps, num_hid]
-        x, _ = self.rnn(x)
-        x = x[:,-1,:].reshape(B,R,-1)
-        return self.fc_out(x).reshape(B, R, self.n_edge_types)
+        x = self.mlp2(x).reshape(B,T,R,self.hidden_dim)
+        h_prev = [torch.zeros((B*R, self.hidden_dim)).to(inputs.device) for _ in range(self.num_rec_layers)]
+        c_prev = [torch.zeros((B*R, self.hidden_dim)).to(inputs.device) for _ in range(self.num_rec_layers)]
+        for t in range(T):
+            prev_input = x[:,t,:,:].reshape(B*R,-1)
+            for i in range(self.num_rec_layers):
+                h_prev[i], c_prev[i] = self.hidden_decoder[i](prev_input, (h_prev[i], c_prev[i]))
+                h_prev[i] = self.edge2node(h_prev[i].reshape(B,R,-1), rel_rec)
+                h_prev[i] = self.mlp3(h_prev[i])
+                h_prev[i] = self.node2edge(h_prev[i], rel_rec, rel_send)
+                h_prev[i] = self.mlp4(h_prev[i]).reshape(B*R,-1)
+                if i==0:
+                    prev_input = torch.cat([x[:,t,:,:].reshape(B*R,-1), h_prev[i]], dim=-1)
+                else:
+                    prev_input = torch.cat([h_prev[i-1], h_prev[i]], dim=-1)
+        x = h_prev[-1].reshape(B,R,-1)
+        return self.fc_out(x).reshape(B, -1, self.n_edge_types)
